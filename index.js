3996019b9640dfce7f3a474450f6453de905dce6
karla.hernandez <elizabeth.lala99@gmail.com>
2022-06-15 00:28:48 -0500

    feat:this is my map

---
+++ index.js
@@ -0,0 +1,13 @@
+//El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.//
+//map//
+// caso de uso: saber cuantos animales ahi en la granja//
+var numbers = [0, 1, 2, 3];
+var doubles = numbers.map(function(x) {
+   return x * 2;
+});
+
+var numbers = [1, 5, 8];
+var roots = numbers.map(function(num) {
+    return Math.sqrt(num);
+});
+console.log(doubles,roots);
